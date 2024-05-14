58253127c38d6efbd7e470fd71a4e648df62b626
vetlelg <61113128+vetlelg@users.noreply.github.com>
2024-05-14 16:29:26 +0200

    tried to change the code, but changed it back. Wasted a lot of time on nothing

--- application.py
+++ application.py
@@ -8,31 +8,33 @@ window_size = 3
 TIMEOUT = 0.5 # 500ms = 0.5s
         
 def server(ip, port, file):
-    seq, ack, flags = None, None, None
+    seq, ack = 0, 0
     server_socket = socket(AF_INET, SOCK_DGRAM)
     server_socket.bind((ip, port))
 
     # Establish connection. Three way handshake
     # Wait for SYN from client
+    expected_packet = create_packet(seq, ack, 8)
     while True:
         packet, addr = server_socket.recvfrom(HEADER_SIZE)
         seq, ack, flags = extract_header(packet)
         # Check if received packet is a SYN packet
-        if flags == 8:
+        if packet == expected_packet:
             print("SYN packet is received")
             break
     
     server_socket.settimeout(timeout)
+    packet = create_packet(ack, seq+1, 12)
+    expected_packet = create_packet()
     while True:
         try:
             # Send SYN-ACK to client
-            server_socket.sendto(create_packet(0, seq+1, 12), addr)
+            server_socket.sendto(packet, addr)
             print("SYN-ACK packet is sent")
             # Wait for ACK from client
             packet = server_socket.recvfrom(HEADER_SIZE)[0]
-            seq, ack, flags = extract_header(packet)
             # Check if received packet is an ACK packet
-            if flags == 4:
+            if packet == expected_packet:
                 print("ACK packet is received")
                 print("Connection established")
                 break
@@ -40,6 +42,7 @@ def server(ip, port, file):
             print("Timeout. ACK packet not received in time")
             continue
     
+    seq, ack = extract_header(packet)
     # Start receiving data
     with open(file, 'wb') as f:
         while True:
@@ -70,84 +73,73 @@ def server(ip, port, file):
             print("Timeout. ACK packet not received in time")
             continue
 
-
-
-def client(ip, port, file):
+def client(addr, file):
     client_socket = socket(AF_INET, SOCK_DGRAM)
 
     # Establish connection. Three way handshake
+    # Send SYN packet to server and receive SYN-ACK packet from server
+    seq, ack = 0, 0
+    packet = create_packet(seq, ack, 8)
+    expected_packet = create_packet(ack, seq+1, 12)
     while True:
         try:
-            # Send SYN packet to server
-            client_socket.sendto(create_packet(0, 0, 8), (ip, port))
+            client_socket.sendto(packet, addr)
             print("SYN packet is sent")
-            # Receive SYN-ACK packet from server
             packet = client_socket.recvfrom(HEADER_SIZE)[0]
-            seq, ack, flags = extract_header(packet)
-            # Check if received packet is SYN-ACK
-            if flags == 12:
+            if packet == expected_packet:
                 print("SYN-ACK packet is received")
                 break
         except socket.timeout:
-            print("Timeout. SYN-ACK packet not received within time limit")
-        
-        # Send ACK packet to server
-        client_socket.sendto(create_packet(ack, seq+1, 4), (ip, port))
-        print("ACK packet is sent")
-
-        # Connection established. Start sending data...
-        print("Data Transfer:")
-        with open(file, 'rb') as f:
-            data = f.read(CHUNK_SIZE)
-            while data:
-                client_socket.sendto(create_packet(ack, seq+1, 4, data), (ip, port)) # Send data
-                packet = client_socket.recvfrom(HEADER_SIZE)[0] # Receive ACK
-                seq, ack, flags = extract_header(packet)
-                if flags == 4:
-                    print("ACK received")
-                    data = f.read(CHUNK_SIZE)   
+            print("ACK not received in time. Retransmitting packet")
+            continue
     
-        # Terminate connection. Four way handshake
-        print("Data transfer finished. Connection Teardown.")
-        while True:
-            try:
-                client_socket.sendto(create_packet(ack, seq, 6), (ip, port)) # Send FIN-ACK
-                print("FIN packet is sent")
-                packet = client_socket.recvfrom(HEADER_SIZE)[0] # Receive ACK
-                seq, ack, flags = extract_header(packet)
-                if flags == 4:
-                    print("ACK packet is received")
-                    break
-            except socket.timeout:
-                print("Timeout. ACK not received")
-                continue
-            
-        while True:
-            packet = client_socket.recvfrom(HEADER_SIZE)[0] # Receive FIN-ACK
-            seq, ack, flags = extract_header(packet)
-            if flags == 6:
-                print("FIN packet is received")
-                client_socket.sendto(create_packet(ack, seq+1, 4), (ip, port)) # Send ACK
-                print("ACK packet is sent")
-                print("Connection closes")
-                client_socket.close()
+    # Return ACK packet to server
+    seq, ack = extract_header(packet)
+    packet = create_packet(ack, seq+1, 4)
+    client_socket.sendto(packet, addr)
+    print("ACK packet is sent")
+    print("Connection established")
 
-def send_packet(socket, packet, expected_ack_packet, addr):
+    # Connection established. Start sending data
+    print("Data Transfer:")
+    with open(file, 'rb') as f:
+        data = f.read(CHUNK_SIZE)
+        while data:
+            packet = create_packet(ack, seq+1, 4, data)
+            expected_packet = create_packet(seq+1, seq+1+CHUNK_SIZE, 4)
+            packet = send_packet(client_socket, addr, packet, expected_packet)
+            seq, ack = extract_header(packet)
+            if expected_packet == packet: data = f.read(CHUNK_SIZE)
+
+    # Terminate connection. Four way handshake
+    # Send FIN-ACK packet and receive ACK packet from server
+    print("Connection Teardown:")
+    packet = create_packet(ack, seq, 6)
+    expected_packet = create_packet(ack, seq+1, 4)
     while True:
         try:
-            socket.sendto(packet, addr)
-            print_packet(packet)
-            packet = socket.recvfrom(HEADER_SIZE)[0]
-            if packet == expected_ack_packet:
-                print_packet(packet)
-                return packet
+            client_socket.sendto(packet, addr)
+            print("FIN packet is sent")
+            packet = client_socket.recvfrom(HEADER_SIZE)[0]
+            if packet == expected_packet:
+                print("ACK packet is received")
+                break
         except socket.timeout:
-            print("Timeout. ACK not received in time.")
-
-def print_packet(packet):
-    seq, ack, flags = extract_header(packet)
-    print(f"seq={seq}, ack={ack}, flags={flags}")
+            print("ACK not received in time. Retransmitting packet")
+            continue
     
+    expected_packet = create_packet(ack, seq+1, 6)
+    while True:
+        packet = client_socket.recvfrom(HEADER_SIZE)[0] # Receive FIN-ACK
+        if packet == expected_packet:
+            print("FIN packet is received")
+            seq, ack = extract_header(packet)
+            packet = create_packet(ack, seq+1, 4)
+            client_socket.sendto(packet, addr) # Send ACK
+            print("ACK packet is sent")
+            print("Connection closes")
+            client_socket.close()
+            break
 
 def create_packet(seq, ack, flags, data = b''):
     return seq.to_bytes(2, 'big') + ack.to_bytes(2, 'big') + flags.to_bytes(2, 'big') + data
@@ -155,8 +147,7 @@ def create_packet(seq, ack, flags, data = b''):
 def extract_header(packet):
     seq = int.from_bytes(packet[:2], 'big')
     ack = int.from_bytes(packet[2:4], 'big')
-    flags = int.from_bytes(packet[4:6], 'big')
-    return seq, ack, flags
+    return seq, ack
 
 
 def parse_arguments():
@@ -192,7 +183,7 @@ def parse_arguments():
     elif args.server:
         server(args.ip, args.port, args.file)
     elif args.client:
-        client(args.ip, args.port, args.file)
+        client((args.ip, args.port), args.file)
 
 def main():
     parse_arguments()
